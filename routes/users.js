2d9cfa3bb8c54be7b71cd46bebc7e6317a446ab4
sajad <masjoodi@chargoon.com>
2023-12-30 21:30:10 +0330

    add route changes

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/createUser', (req, res, next) => {
+  /// Add Create User 
+  res.send('not implemented')
+});
+
+router.get('/createUser', (req, res, next) => {
+  /// Add Create User 
+  res.send('not implemented cause its Unnecessary ')
+});
+
+router.get('/getUsersDoneWork', (req, res, next) => {
+  /// Add Get User's doneWork
+  res.send('not implanted')
 });
 
 module.exports = router;
